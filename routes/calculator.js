2314e744ef88169a96870343147141c1812f0a65
KennethEllebring <kenneth.ellebring@yh.nackademin.se>
2023-05-15 10:17:40 +0200

    moved calculator from route to controller

--- routes/calculator.js
+++ routes/calculator.js
@@ -1,39 +1,11 @@
 const express = require('express');
 
+const Calculator = require('../controllers/calculator');
+
 const router = express.Router();
 
-class Calculator {
-  constructor() {
-    this.result = 0;
-  }
-
-  getResult() {
-    return this.result;
-  }
-
-  add(x, y) {
-    this.result = x + y;
-    return this.result;
-  }
-
-  remove(x, y) {
-    this.result = x - y;
-    return this.result;
-  }
-
-  multiply(x, y) {
-    this.result = x * y;
-    return this.result;
-  }
-
-  divide(x, y) {
-    this.result = x / y;
-    return this.result;
-  }
-}
-
-router.get('/', (req, res, next) => {
-  res.send('hello');
+router.get('/', Calculator, (req, res, next) => {
+  res.send('Add calculator');
 
   module.exports = router;
 });
